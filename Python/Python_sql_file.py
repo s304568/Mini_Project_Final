77d35ca0c3fb608cd61061e259eb6c989cdd1e18
Nikola Todorov <s304568@student.salfordcc.ac.uk>
2024-01-10 12:16:40 +0000

    python second

--- Python/Python_sql_file.py
+++ Python/Python_sql_file.py
@@ -9,20 +9,22 @@ from bcrypt import checkpw, gensalt, hashpw
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
-conn = sqlite3.connect(r"C:\Users\Nikola\Desktop\web_dev\react\T-level mini-2\Python\UserName-PassWord.db",check_same_thread=False)
+conn = sqlite3.connect(r"G:\vb\ALEX_vb\React\Mini_Project_Final\Python\UserName-PassWord.db",check_same_thread=False)
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-with open(r"C:\Users\Nikola\Desktop\web_dev\react\T-level mini-2\Python\OneJson.json","r") as file:
+with open(r"G:\vb\ALEX_vb\React\Mini_Project_Final\Python\OneJson.json","r") as file:
     j2 = json.load(file)
     
 @app.route('/signup', methods=['POST'])
 def signup():
     data = request.json
-    newUsername = data.get('newUsername')
-    newPassword = data.get("finalPassword")
-    conn = sqlite3.connect(r"C:\Users\Nikola\Desktop\web_dev\react\T-level mini-2\Python\UserName-PassWord.db",check_same_thread=False)
+    newUsername = data.get('username')
+    newPassword = data.get("password")
+    print(newUsername)
+    print(newPassword)
+    conn = sqlite3.connect(r"G:\vb\ALEX_vb\React\Mini_Project_Final\Python\UserName-PassWord.db",check_same_thread=False)
     cu = conn.cursor()
     validName = False 
     validPass = False
@@ -32,16 +34,17 @@ def signup():
 
     resultName = re.match("^[a-zA-Z]+$", newUsername)
     resultPass = re.match("(?=.*[a-zA-Z])(?=.*\d)", newPassword)
+#elif resultPass:
+ #       validPass = True
 
     if len(newUsername) >= 5 and len(newUsername) <= 16:
         validName = True
-    elif resultName:
-        validName = True
-    else: validName = False
+    elif resultPass:
+       validPass = True
+    else: 
+        validName = False
 
     if len(newPassword) >= 5 and len(newPassword) <= 16:
-        validPass = True
-    elif resultPass:
         validPass = True
     else:
         validPass = False
@@ -50,9 +53,11 @@ def signup():
         cu.execute(q, q_entry)
         conn.commit()
         message = "Valid Sign-Up !!!"
+        print("This is the message___", message)
         return jsonify({"message":message})
     else:
         message = "Invalid Sign-Up. Please try again !!!"
+        print("This is the message___", message)
         return jsonify({"message":message})
     
 
@@ -61,7 +66,9 @@ def signup():
 def login():
     data = request.json
     username = data.get('username')
-    password = data.get('password') 
+    password = data.get('password')
+    print("prints username____:",username)
+    print("prints password____:",password) 
 
     Final_q= check_credentials(username, password)
     #print("Final q: " + Final_q)
